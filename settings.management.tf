e2239a109fee5a0a6711ab801413ac1df0cef11f
Bilal Amjad <79409563+mbilalamjad@users.noreply.github.com>
2024-03-01 07:37:12 -0800

    Update settings.management.tf

--- settings.management.tf
+++ settings.management.tf
@@ -24,7 +24,7 @@ locals {
       security_center = {
         enabled = true
         config = {
-          email_security_contact                                = "admin@MngEnvMCAP691735.onmicrosoft.com"
+          email_security_contact                                = "securityalerts@contoso.com"
           enable_defender_for_apis                              = true
           enable_defender_for_app_services                      = true
           enable_defender_for_arm                               = true
